958f9dad7c88573243d6f10c2edc726c6ae57ed0
Cédric Pahud <cedric.pahud@he-arc.ch>
2018-01-12 11:30:39 +0100

    séparation du register

--- app/src/main/java/ch/he_arc/inf3dlm_a/geowalk/RegisterActivity.java
+++ app/src/main/java/ch/he_arc/inf3dlm_a/geowalk/RegisterActivity.java
@@ -6,60 +6,54 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.ProviderQueryResult;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import org.w3c.dom.Text;
-
-import java.util.Map;
-
-
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseUser user;
 
     private String email;
     private String password;
+    private String username;
 
     private TextView error_short;
-    private TextView error_incorrect;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
-        error_incorrect = findViewById(R.id.error_password_incorrect);
         error_short = findViewById(R.id.error_password_short);
 
-        findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.create_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 error_short.setVisibility(View.GONE);
-                error_incorrect.setVisibility(View.GONE);
-                if(getPasswordEmail()){
-                    mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                if(getPasswordEmailUsername()){
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
+                            if(task.isSuccessful()){
                                 user = mAuth.getCurrentUser();
+                                if(user != null){
+                                    String userId = user.getUid();
+                                    DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+                                    database.child("users").child(userId).setValue(new User(username));
+                                }
                                 openMap();
-                            } else {
-                                error_incorrect.setVisibility(View.VISIBLE);
+                            }
+                            else{
+
                             }
                         }
                     });
@@ -69,33 +63,6 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-
-        findViewById(R.id.create_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    protected boolean getPasswordEmail(){
-
-        email = ((EditText)findViewById(R.id.email)).getText().toString();
-        password = ((EditText)findViewById(R.id.password)).getText().toString();
-
-        if(password.length() < 8 ){
-            return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        user = mAuth.getCurrentUser();
-        openMap();
     }
 
     private void openMap(){
@@ -105,5 +72,18 @@ public class LoginActivity extends AppCompatActivity {
             intent.putExtra("user_id",user.getUid());
             startActivity(intent);
         }
+    }
+
+    protected boolean getPasswordEmailUsername(){
+
+        username = ((EditText)findViewById(R.id.username)).getText().toString();
+        email = ((EditText)findViewById(R.id.email)).getText().toString();
+        password = ((EditText)findViewById(R.id.password)).getText().toString();
+
+        if(password.length() < 8 ){
+            return false;
+        }
+
+        return true;
     }
 }
